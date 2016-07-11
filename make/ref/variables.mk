e3c4cd8cfb7122636a4b030956b5372b64955b03
Amerlyq <amerlyq@gmail.com>
2016-07-11 04:39:34 +0300

    [make] Snip: order-only

--- make/ref/variables.mk
+++ make/ref/variables.mk
@@ -4,3 +4,11 @@ target:
 
 	## Build cmake
 	# $(CMAKE) -E chdir $(O) $(CMAKE) $(shell pwd)
+
+
+# Use ordered-only prq to ignore dir's timestamp
+# NOTE: works only when CACHE is the same dir (not template)
+cmake/%: | $(CACHE)
+
+$(CACHE):
+	mkdir -p $(CACHE)
