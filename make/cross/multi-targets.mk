96651240e6082f5e50336e00875915d727717c49
Amerlyq <amerlyq@gmail.com>
2016-08-16 22:36:35 +0300

    [make] enh passing targets

--- make/cross/multi-targets.mk
+++ make/cross/multi-targets.mk
@@ -4,31 +4,49 @@
 # NOTE: can be extracted to '-include main.mk'
 ifeq ($(MAKELEVEL),0)
 O ?= _build
-PLTF := linux windows android
+PLTF ?= linux windows android
+.DEFAULT_GOAL = linux
+# .DEFAULT_GOAL = all
 
-default: linux
-
-Makefile: ;
-%.mk :: ;
-$(O)/% :: ; +@mkdir -p "$@"
-
-.PHONY: $(PLTF)
-$(PLTF): % : | $(O)/%
+.PHONY: $(PLTF) all
+all: $(PLTF)
+$(PLTF:%=$(O)/%) :: ; +@mkdir -p "$@"
+$(PLTF) :: % : | $(O)/%
 	+@$(MAKE) --no-print-directory -C "$|" -f "$(shell pwd)/Makefile" \
-	  $(patsubst $@/%,%,$(filter $@/%,$(MAKECMDGOALS)))
+	  $(filter-out $(PLTF) $(PLTF:%=%/%),$(MAKECMDGOALS:$@/%=%))
+# ALT:(don't pass simple) $(patsubst $@/%,%,$(filter $@/%,$(MAKECMDGOALS)))
+# NOTE: in general can't use $(shell pwd) -- if this file was included itself
 
-.SECONDEXPANSION:
-$(PLTF:%=%/%) :: $$(firstword $$(subst /, ,$$@)) ; :
+Makefile :: ;
+%.mk :: ;
+# OR:(~) .DEFAULT: ...
+% :: $(.DEFAULT_GOAL) ;
+$(foreach p,$(PLTF),$(eval $p/% :: $p ;))
+
+# WARN: if targets contain '%' -> Make execs recipe only once to make them all!
+# ALT: .SECONDEXPANSION:
+# $(PLTF:%=%/%) :: $$(firstword $$(subst /, ,$$@)) ; @:
+# BAD: manual targets don't appear in MAKECMDGOALS
+# BAD: terminating rule '% ::' don't work for PHONY
+
 else
 ##################### Target #####################
-default: build test
+.DEFAULT_GOAL = all
 
 PLTF := $(notdir $(shell pwd))
 MWD := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 AUX := $(MWD)/bin
+
+# WARN: exporting pollutes PATH for all subsequent toolchains !
 export PATH := $(AUX):$(PATH)
 
+all: build test
 build:
 	build_$(PLTF)
 
+clean:
+	@rm -vrf ./*
+	@echo "clean: $(PLTF)"
+
+.PHONY: all build clean
 endif
--- make/doc/functions.mk
+++ make/doc/functions.mk
@@ -13,3 +13,15 @@ $(info $$ ${1} -- [${2}/${3}])
 $(1): $(2)/$(3)
 endef
 $(call t_custom,b,trg,dbg)
+
+
+# Multiple targets from one rule
+## With common
+%.out1 %.out2: %.input
+    foo-bin $*.input $*.out1 $*.out2
+
+## Completely different names (ALSO portable to non-GNU Make)
+file-a.out file-b.out: input.in.intermediate
+.INTERMEDIATE: input.in.intermediate
+input.in.intermediate: input.in
+    foo-bin input.in file-a.out file-b.out
