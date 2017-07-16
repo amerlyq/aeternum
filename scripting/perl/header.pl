e9f5f672d8cea288c8a4443f8c48799528868cb6
Amerlyq <amerlyq@gmail.com>
2017-07-16 04:04:27 +0300

    [perl] fix common header

--- scripting/perl/header.pl
+++ scripting/perl/header.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl
 # Permanent header for all scripts
 use warnings FATAL => 'all';  # treat all warnings as fatal, always die
+use autodie qw(:all);         # similar to 'bash -e' -- immediate exit on error
 use bigint qw(hex);           # correct 'hex()' for numbers > 32bit
-use autodie;                  # similar to 'bash -e' -- immediate exit on error
 use strict;                   # more warnings for loose/misused syntax
 use utf8;                     # non-ASCII support in script/stdin/stdout
 
 # WARN: 'perl -w' != 'use warnings;'
 #   https://perlmaven.com/always-use-strict-and-use-warnings
+# WARN: 'autodie' don't support exotic 'system { $cmd } @args';
+#   http://search.cpan.org/~pjf/autodie-2.29/lib/autodie.pm
+#   https://stackoverflow.com/questions/3941632/are-there-disadvantages-to-autodie
 
 use v5.10; # Only needed for 'static vars' -- initialized only once
 # E.G. 'state ($var) = 1'
