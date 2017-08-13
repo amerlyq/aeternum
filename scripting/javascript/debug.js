0dd21919efdb63f541da4397ae671d7cf888716f
Amerlyq <amerlyq@gmail.com>
2017-08-14 01:59:03 +0300

    [js] debug+

--- scripting/javascript/debug.js
+++ scripting/javascript/debug.js
@@ -1,3 +1,7 @@
 // Debug
 window.alert(loadPos());
 window.alert('S: ' + y + '/' + h + ' : ' + pos);
+
+// ++ DEBUG methods <C-S-j>
+// https://developer.mozilla.org/en-US/docs/Mozilla/Debugging/Debugging_JavaScript
+// https://developer.mozilla.org/en-US/docs/Web/API/console
